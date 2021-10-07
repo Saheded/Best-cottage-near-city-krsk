283c1a8da7b8655452545313d6bd6ff65b9930b4
Федя <Saheded@mail.ru>
2021-10-08 05:11:48 +0700

    Media update

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-;
+(function () {
+    'use strict';
+
+    function trackScroll() {
+        var scrolled = window.pageYOffset;
+        var coords = document.documentElement.clientHeight;
+
+        if (scrolled > coords) {
+            goTopBtn.classList.add('up-button-show');
+        }
+        if (scrolled < coords) {
+            goTopBtn.classList.remove('up-button-show');
+        }
+    }
+
+    function backToTop() {
+        if (window.pageYOffset > 0) {
+            window.scrollBy(0, -80);
+            setTimeout(backToTop, 0);
+        }
+    }
+
+    var goTopBtn = document.querySelector('.up-button');
+
+    window.addEventListener('scroll', trackScroll);
+    goTopBtn.addEventListener('click', backToTop);
+})();
